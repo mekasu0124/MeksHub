b512094c199fa70aeaaf5f7a4b279553f2964d4c
Mekasu <mekasurenae@gmail.com>
2024-10-10 17:35:26 -0500

    created dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,19 @@
-export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Mek's Hub - Dashboard",
 }
+
+export default function DashboardPageLayout({
+  children
+}: Readonly<{ 
+  children: React.ReactNode 
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+};
