ccc98b40ffcf8f3798ac82b27ce7b6da5a2e2363
Mekasu <mekasurenae@gmail.com>
2024-10-15 16:36:59 -0500

    created overall layout (changed browser tab title) per page

---
+++ src/app/(auth)/forgot/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mek's Hub - Forgot Password",
+  description: "Developed By Mek & Friends",
+  icons: ["/images/app-icon.jpg"]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
