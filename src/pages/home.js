e6a5c28d5c243b9efc3557400c39257bcf3dd13d
Mekasu <mekasurenae@gmail.com>
2024-10-17 19:07:59 -0500

    started home page

---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+import { useAuth } from "../hooks/authContext";
+import { useNavigate } from "react-router-dom";
+
+import Header from '../components/home/header';
+import Main from '../components/home/main';
+import Footer from '../components/home/footer';
+
+export default function Home() {
+  const { user } = useAuth();
+  const navigate = useNavigate('');
+
+  if (!user) {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Header />
+
+        <Main />
+
+        <Footer />
+      </div>
+    )
+  }
+
+  return navigate('/dashboard');
+};
