0b59ac58cf81eccadb628884d7ab945517f1ace5
Mekasu <mekasurenae@gmail.com>
2024-10-17 19:06:54 -0500

    started routes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/authContext';
+
+import Home from './pages/home';
+import SignUp from './pages/auth/signup';
+import Login from './pages/auth/login';
+import ForgotPassword from './pages/auth/forgot';
+import Dashboard from './pages/dashboard/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/auth">
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot" element={<ForgotPassword />} />
+          </Route>
+
+          <Route path="/hub">
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
