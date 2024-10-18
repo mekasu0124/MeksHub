68d53236989598267126d668c303c8a06086a0cd
Mekasu <mekasurenae@gmail.com>
2024-10-17 19:08:05 -0500

    started page

---
+++ src/pages/auth/forgot.js
@@ -0,0 +1,9 @@
+import Title from '../../components/universal/title';
+
+export default function ForgotPassword() {
+  return (
+    <div className="flex flex-col w-full min-h-screen">
+      <Title props={{ title: "Reset Your Password" }} />
+    </div>
+  );
+};
