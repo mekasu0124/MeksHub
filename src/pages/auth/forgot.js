df03aa044e2c8624e65b5417c0da51c2dcc25fec
Mekasu <mekasurenae@gmail.com>
2024-10-18 19:25:19 -0500

    added title

--- src/pages/auth/forgot.js
+++ src/pages/auth/forgot.js
@@ -4,6 +4,7 @@ export default function ForgotPassword() {
   return (
     <div className="flex flex-col w-full min-h-screen">
       <Title props={{ title: "Reset Your Password" }} />
+      <Title props={{ title: "I have zero idea how to build this functionality. Will proceed in the future." }} />
     </div>
   );
 };
