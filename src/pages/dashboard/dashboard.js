ce01e0ae69a7d076ccd7d173380242b15411ea60
Mekasu <mekasurenae@gmail.com>
2024-10-18 19:25:44 -0500

    I just don't know here. it is what it is at this point

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useAuth } from '../../hooks/authContext';
 
-function dashboard() {
+import Title from '../../components/universal/title';
+
+export default function Dashboard() {
+  const [user, setUser] = useState(null);
+
+  const { logout } = useAuth();
+  const navigate = useNavigate('');
+
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedUser'));
+    console.log(loggedInUser);
+
+    setUser(loggedInUser);
+  }, []);
+
   return (
-    <div>dashboard</div>
-  )
-}
+    <div className="flex flex-col w-full min-h-screen">
+      <Title props={{ title: `${user.username}'s Dashboard` }} />
 
-export default dashboard
+      <div className="flex flex-col items-center justify-center w-full flex-1 mt-5">
+        <button onClick={() => logout()} className="w-20 h-20 bg-white">Logout</button>
+      </div>
+    </div>
+  );
+};
