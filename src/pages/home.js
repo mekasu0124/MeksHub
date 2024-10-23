5d39d470007fbbad94556f6025b1e6590462db9f
Mekasu <mekasurenae@gmail.com>
2024-10-22 22:05:22 -0500

    updated file to check for user being remembered and navigate appropriately

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,12 +4,19 @@ import { useNavigate } from "react-router-dom";
 import Header from '../components/home/header';
 import Main from '../components/home/main';
 import Footer from '../components/home/footer';
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { user } = useAuth();
   const navigate = useNavigate('');
 
-  if (!user) {
+  const [currentUser, setCurrentUuser] = useState(null);
+
+  useEffect(() => {
+    setCurrentUuser(user);
+  }, [user]);
+
+  if (!currentUser) {
     return (
       <div className="flex flex-col min-h-screen">
         <Header />
@@ -21,5 +28,8 @@ export default function Home() {
     )
   }
 
-  return navigate('/hub/dashboard');
+  if (user.isRemembered) {
+    console.log(user.isRemembered);
+    return navigate('/hub/dashboard');
+  };
 };
